6a727fb965aed77c19c194326c1c38e12c390096
Ausrys Work <ausrys.mikoliunas@orioninc.com>
2025-04-18 12:57:01 +0300

    linting errors

--- Terraform/main.py
+++ Terraform/main.py
@@ -1,14 +1,14 @@
 import json
 
 
-def lambda_handler(event, context):
+def lambda_handler(event, _):
     try:
         body = json.loads(event["body"]) if event.get("body") else {}
         return {
             "statusCode": 200,
             "body": json.dumps(body)
         }
-    except Exception as e:
+    except RuntimeError as e:
         return {
             "statusCode": 500,
             "body": json.dumps({"error": str(e)})
